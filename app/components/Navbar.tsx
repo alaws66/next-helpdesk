ad90773681cbc2f71c592cc161cdcfa4e023314c
Aimee Laws <aimee.laws13@gmail.com>
2023-09-19 15:09:19 +0100

    navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '@/public/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Image
+        src={Logo}
+        alt="Helpdesk logo"
+        width={70}
+        placeholder="blur"
+        quality={100}
+      />
+      <h1>Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { ReactNode } from 'react'
 
+// components
+import Navbar from './components/Navbar';
+
 const poppins = Poppins({
   weight: ['400', '600'],
   subsets: ['latin'],
@@ -21,8 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
