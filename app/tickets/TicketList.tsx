c074d05e3520475b8d85a4ea924e4f00beed836e
Aimee Laws <aimee.laws13@gmail.com>
2023-10-02 14:31:37 +0100

    added individual ticket page

--- app/tickets/TicketList.tsx
+++ app/tickets/TicketList.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 // fetch json data
 const getTickets = async () => {
   const res = await fetch('http://localhost:4000/tickets', {
@@ -25,11 +27,13 @@ export default async function TicketList() {
       {/* display tickets */}
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
