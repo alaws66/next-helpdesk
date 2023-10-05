8bf42122aaa00ce1a5c3eb100f3110efaa47d012
Aimee Laws <aimee.laws13@gmail.com>
2023-10-05 12:04:54 +0100

    login functionality

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,43 @@
+'use client'
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+// components
+import AuthForm from '../AuthForm';
+
 export default function Login() {
+  const router = useRouter();
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>, email: string, password: string) => {
+    e.preventDefault();
+    setFormError('');
+
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+
+    if (error) {
+      setFormError(error.message);
+    } else {
+      router.push('/');
+    }
+  }
+
   return (
     <main>
       <h2 className="text-center">Log in</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {formError &&
+        <div className="error">{formError}</div>
+      }
     </main>
   );
 }
